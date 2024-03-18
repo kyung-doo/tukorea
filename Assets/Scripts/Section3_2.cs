5d36fdf38da6bbcfbdb582910dda52d76c22bba2
DESKTOP-79D198C\Srweb_Dev <hohomkd@naver.com>
2024-03-18 10:46:45 +0900

    no message

--- Assets/Scripts/Section3_2.cs
+++ Assets/Scripts/Section3_2.cs
@@ -15,6 +15,13 @@ public class Section3_2 : SectionBase
     [SerializeField]
     public GameObject touch1;
 
+    [SerializeField]
+    public GameObject touch2;
+
+
+    [SerializeField]
+    public GameObject screen2;
+
 
     private Vector3 zoomPlayerPos = new Vector3(-2.06505466f, 0.2282888f, 4.67137241f);
     private Vector3 zoomPlayerRo = new Vector3(0f, -89.8f, 0f);
@@ -31,6 +38,7 @@ public class Section3_2 : SectionBase
     {
         Debug.Log("startSection2");
         touch1.SetActive(false);
+        touch2.SetActive(false);
         coTimer = Start1();
         StartCoroutine(coTimer);
         
@@ -53,6 +61,26 @@ public class Section3_2 : SectionBase
         touch1.SetActive(false);
         touch1.GetComponent<Clickable>().onMouseClick -= ClickTouch1;
         touch1.GetComponent<Animator>().Rebind();
+        screen2.SetActive(true);
+        coTimer = Start2();
+        StartCoroutine(coTimer);
+    }
+
+    private IEnumerator Start2() 
+    {
+        yield return new WaitForSeconds( 1f );
+        Main.Instance.PlayAudio(audioClips[1], () => {
+            touch2.SetActive(true);
+            touch2.GetComponent<Animator>().Play("touch");
+            touch2.GetComponent<Clickable>().onMouseClick += ClickTouch2;
+        });
+    }
+
+    private void ClickTouch2 ( GameObject target, Vector3 mousePos ) 
+    {
+        touch2.SetActive(false);
+        touch2.GetComponent<Clickable>().onMouseClick -= ClickTouch2;
+        touch2.GetComponent<Animator>().Rebind();
         // coTimer = Start2();
         // StartCoroutine(coTimer);
     }
@@ -84,6 +112,8 @@ public class Section3_2 : SectionBase
         // animator.Rebind();
         // animator.Update(0f);
         touch1.GetComponent<Clickable>().onMouseClick -= ClickTouch1;
+        touch1.GetComponent<Clickable>().onMouseClick -= ClickTouch2;
+        screen2.SetActive(false);
         
        
         if(coTimer != null) StopCoroutine(coTimer);
